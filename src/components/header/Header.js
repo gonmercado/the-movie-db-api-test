a74037d7caaef8c6a193e4b05e76f494ef43409d
Gonzalo Mercado <gonmercado@gmail.com>
2019-08-30 22:16:57 -0300

    Cleaning CRA and adding initial file structure

---
+++ src/components/header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+      Application Header
+  </header>
+);
+
+export default Header;
---
+++ src/components/homePage/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './homePage.css';
+
+const HomePage = () => (
+    <div>
+        Home Page
+    </div>
+);
+
+export default HomePage;
